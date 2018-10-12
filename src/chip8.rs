3b503778c981797222d7c98634adb258dcdefc5c
Christopher Medlin <christopherjmedlin@gmail.com>
2018-10-12 13:21:08 -0700

    Implement and test 5xy0

--- src/chip8.rs
+++ src/chip8.rs
@@ -9,6 +9,7 @@ pub struct Chip8 {
     i: usize,
     pc: usize,
     sp: usize,
+    should_increment_pc: bool
 }
 
 impl Chip8 {
@@ -21,7 +22,8 @@ impl Chip8 {
             v: [0; 16],
             i: 0,
             pc: 0x200,
-            sp: 0
+            sp: 0,
+            should_increment_pc: false
         };
         for (i, &font) in fonts::FONTS.iter().enumerate() {
             cpu.ram[i] = font;     
@@ -50,57 +52,80 @@ impl Chip8 {
         let y = nibbles.2 as usize;
         let n = nibbles.3 as usize;
         
-        match nibbles {
+        let pc_change = match nibbles {
             (0x0, 0x0, 0xE, 0x0) => self.op_00E0(),
             (0x0, 0x0, 0xE, 0xE) => self.op_00EE(),
             (0x1, _, _, _) => self.op_1nnn(nnn),
             (0x2, _, _, _) => self.op_2nnn(nnn),
             (0x3, _, _, _) => self.op_3xkk(x, kk),
             (0x4, _, _, _) => self.op_4xkk(x, kk),
-            _ => println!("Unimplemented opcode: {}", opcode)
-        }
+            (0x5, _, _, 0) => self.op_5xy0(x, y),
+            _ => self.unimplemented(opcode)
+        };
+
+        self.pc += pc_change;
     }
 
     // Returns the opcode at the program counter
     fn get_opcode(&mut self) -> u16 {
-        return (self.ram[self.pc] as u16) << 8 | (self.ram[self.pc + 1] as u16)
+        (self.ram[self.pc] as u16) << 8 | (self.ram[self.pc + 1] as u16)
     }
     
     // Clear display
-    fn op_00E0(&mut self) {
+    fn op_00E0(&mut self) -> (usize) {
         //unimplemented
+        2
     }
     
     // Return from subroutine
-    fn op_00EE(&mut self) {
+    fn op_00EE(&mut self) -> (usize) {
         self.pc = self.stack[self.sp];
         self.sp -= 1;
+        0
     }
     
     // Jump to address nnn
-    fn op_1nnn(&mut self, nnn: usize) {
+    fn op_1nnn(&mut self, nnn: usize) -> (usize) {
         self.pc = nnn;
+        0
     }
     
     // Jump to subroutine at nnn
-    fn op_2nnn(&mut self, nnn: usize) {
+    fn op_2nnn(&mut self, nnn: usize) -> (usize) {
         self.sp += 1;
-        self.stack[self.sp] = self.pc;
+        // store next instruction address on stack
+        self.stack[self.sp] = self.pc + 2;
         self.pc = nnn;
+        0
     }
 
     // Skip if Vx equals kk
-    fn op_3xkk(&mut self, x: usize, kk: u8) {
+    fn op_3xkk(&mut self, x: usize, kk: u8) -> (usize) {
         if self.v[x] == kk {
-            self.pc += 2;
+            return 4
         }
+        2
     }
 
     // Skip if Vx DOESNT equal kk
-    fn op_4xkk(&mut self, x: usize, kk: u8) {
+    fn op_4xkk(&mut self, x: usize, kk: u8) -> (usize) {
         if self.v[x] != kk {
-            self.pc += 2;
+            return 4
         }
+        2
+    }
+
+    // Skip if register x equals register y
+    fn op_5xy0(&mut self, x: usize, y: usize) -> (usize) {
+        if self.v[x] == self.v[y] {
+            return 4
+        }
+        2
+    }
+
+    fn unimplemented(&mut self, opcode: u16) -> (usize) {
+        println!("Unimplemented opcode: 0x{:x}", opcode);
+        2
     }
 }   
 
@@ -158,10 +183,10 @@ mod tests {
         // jump to subroutine at 0x205
         chip8.run_opcode(0x2205);
         assert_eq!(chip8.pc, 0x205);
-        assert_eq!(chip8.stack[1], 0x200);
+        assert_eq!(chip8.stack[1], 0x202);
 
         chip8.run_opcode(0x00EE);
-        assert_eq!(chip8.pc, 0x200);
+        assert_eq!(chip8.pc, 0x202);
         assert_eq!(chip8.sp, 0);
     }
 
@@ -181,21 +206,39 @@ mod tests {
         // not equal
         chip8.run_opcode(0x3003);
         // shouldn't skip
-        assert_eq!(chip8.pc, 0x200);
+        assert_eq!(chip8.pc, 0x202);
         
         // equal
         chip8.run_opcode(0x3002);
         // should skip
-        assert_eq!(chip8.pc, 0x202);
+        assert_eq!(chip8.pc, 0x206);
 
         // not equal
         chip8.run_opcode(0x4003);
         // should skip
-        assert_eq!(chip8.pc, 0x204);
+        assert_eq!(chip8.pc, 0x20a);
 
         // equal
         chip8.run_opcode(0x4002);
         // shouldn't skip
-        assert_eq!(chip8.pc, 0x204);
+        assert_eq!(chip8.pc, 0x20c);
+    }
+
+    #[test]
+    fn test_5xy0() {
+        let mut chip8 = Chip8::new();
+        chip8.v[0] = 5;
+        chip8.v[1] = 0;
+        chip8.v[2] = 5;
+        
+        // not equal
+        chip8.run_opcode(0x5010);
+        // shouldn't skip
+        assert_eq!(chip8.pc, 0x202);
+
+        // equal
+        chip8.run_opcode(0x5020);
+        // should skip
+        assert_eq!(chip8.pc, 0x206);
     }
 }
